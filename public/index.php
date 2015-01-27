f90e0ab6b6342ef074586b696d759a9c7e59cf1a
Frostbite <Frostbite@Frostbite-PC>
2015-01-27 21:00:52 +0100

    Functions.php + index.php. Database connection function + UFERDIG make_new_DB_for_user!!!

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php require_once("../includes/functions.php") ?>
+<?php connect_to_db();?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Min Cosmopharma Vareliste</title>
+	</head>
+	<body>
+	
+	</body>
+</html>
