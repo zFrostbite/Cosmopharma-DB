b6ac9fbb16631a10e3669492cbd21eaeaff2b4bc
Frostbite <Frostbite@Frostbite-PC>
2015-01-30 19:07:21 +0100

    ymse systemers

--- includes/functions.php
+++ includes/functions.php
@@ -1,9 +1,10 @@
 <?php
+	
 	function form_errors($error_array=array()) {
 		$output = "";
 		if(!empty($error_array)){
 			$output 	 = 	"<div class=\"Error\">";
-			$output 	.= 	"Vennligst fiks følgende feil";
+			$output 	.= 	"Vennligst fiks følgende feil:";
 			$output 	.="<ul>";
 			foreach($error_array as $key => $error) {
 				$output 	.="<li>{$error}</li>";
@@ -12,5 +13,63 @@
 			$output 	.="</div>";
 		}
 		return $output;
+	}
+	
+	function show_all_consultants()
+	{
+		global $connection;
+		$query = 	"SELECT * ";
+		$query .=	"FROM users ";
+		$query .=	"ORDER BY brukernavn ASC";
+		$result = mysqli_query($connection, $query);
+		confirm_query($result);
+		return $result;
+	}
+	
+	function log_in_user($brukernavn)
+	{
+		$_SESSION["Innlogget"] = TRUE;
+	}
+	
+	function er_innlogget() 
+	{
+		return isset($_SESSION['Innlogget']);
+	}
+	
+	function log_out_user($brukernavn)
+	{
+		
+	}
+	function confirm_query($result_set)
+	{
+		if(!$result_set) {
+			die("Database query failed.");
+		}
+	}
+	
+	function redirect_to($new_location) {
+		header("Location: " . $new_location);
+		exit;
+	}
+	
+	function has_presence($string)
+	{
+		return isset($string) && $string !== "";
+	}
+	
+	function validate_max_lengths($fields_with_max_lengths) {
+		global $errors;
+		global $max;
+		//Using an assoc. array
+		foreach($fields_with_max_lengths as $field => $max) {
+			$value = trim($_POST[$field]);
+			if(!has_max_length($value, $max) && !has_presence($value)) {
+				$errors[$field] = ucfirst($field) . " is too long";
+			}
+		}
+	}
+	
+	function has_max_length($value, $max) {
+		return strlen($value) <= $max;
 	}
 ?>
--- public/index.php
+++ public/index.php
@@ -1,29 +1,63 @@
+<?php session_start();?>
 <?php header('Content-Type: text/html; charset=ISO-8859-1')?>
 <?php require_once("../includes/db_connection.php")?>
 <?php require_once("../includes/functions.php") ?>
+<link rel="stylesheet" href="stylesheets/public.css">
 <?php 
+	if($_SESSION["Innlogget"])
+	{
+		redirect_to("oversikt.php");
+	}else{
+		redirect_to("index.php");
+	}	
 	$errors = array();
 	$message = "";
-	if(isset($_POST['submit']))
+	if(isset($_POST["submit"]))
 	{
 		//Har trykt "Logg inn"
-		$username = trim($_POST['brukernavn']);
-		$password = trim($_POST['passord']);
-		
+		$Brukernavn = trim($_POST["Brukernavn"]);
+		$Passord = trim($_POST["Passord"]);
+		print_r($_POST);
 		//validering
-		$required_fields = array("brukernavn", "passord");
+		$required_fields = array("Brukernavn", "Passord");
 		foreach($required_fields as $field) {
 			$value = trim($_POST[$field]);
-			if(!isset($required_field) && $required_fields !== "") {
-				$errors[$field] = "Vennligst skriv inn ditt " . ucfirst($field);
+			if(!has_presence($value)) {
+				$errors[$field] = "Vennligst skriv inn ditt " . $field;
 			}
-			
 		}
-	}
+		//$fields_with_max_length = array("Brukernavn" => 30, "Passord" => 30);
+		//validate_max_lengths($fields_with_max_length);
+		if(empty($errors))
+		{
+			//Forsøk på å logge inn.
+			$query = "SELECT * ";
+			$query .= "FROM users ";
+			$query .= "WHERE Brukernavn = '{$Brukernavn}' ";
+			$query .= "AND Passord = '{$Passord}' ";
+			echo $query;
+			$message = "Brukernavn- og/eller passordet du skrev stemmer ikke overens med vår database!";
+			mysqli_real_escape_string($connection, $query);
+			$result = mysqli_query($connection, $query);
+
+			foreach($result as $row)
+			{
+				if(!$row["Brukernavn"] || !$row["Passord"] )
+				{
+					$message = "Brukernavn- og/eller passordet du skrev stemmer ikke overens med vår database!";
+				}elseif($row["Brukernavn"] == $Brukernavn && $row["Passord"] == $Passord){
+					$_SESSION["Innlogget"] = TRUE;
+					redirect_to("oversikt.php");
+				}
+			}
+		}  
+	}else{
+		$Brukernavn = "";
+		$message = "Vennligst logg inn.";
+	}				 
 
 
 ?>
-<link rel="stylesheet" href="stylesheets/public.css">
 <!DOCTYPE html>
 <html>
 	<head>
@@ -36,11 +70,13 @@
 	 	</div>
 	 	
 	 	<div id="main">  
-		 	<form action="index.php" method="post"> 
+		 	 
 		 	<center>
+		 	<h4><?php echo $message;?></h4>
 		 	<?php echo form_errors($errors); ?>
-		 	Brukernavn: <input type="text" name="brukernavn" value=""> </br>
-			Passord:	<input type="password" name="passord" value=""> </br></br>
+		 	<form action="index.php" method="post">
+		 	Brukernavn: <input type="text" name="Brukernavn" value="<?php echo htmlspecialchars($Brukernavn); ?>"> </br>
+			Passord:	<input type="password" name="Passord" value=""> </br></br>
 			<input type="submit" name="submit" value="Logg inn">
 			
 		 	</center>	
